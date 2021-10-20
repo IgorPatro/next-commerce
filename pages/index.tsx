00505bd39f7ca7dd4a1df0c7984b13bc8a43b887
Igor Patro <tools@skyagency360.pl>
2021-10-20 19:12:52 +0200

    Enviroment config created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 
-const Home = () => {
-  return <h1>Hello world!</h1>
+interface Props {
+  prop: string
+}
+
+const Home: React.FC<Props> = ({ prop }) => {
+  return <h1>Hello world! {prop}</h1>
 }
 
 export default Home
