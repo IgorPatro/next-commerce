b404d9fcd4e510653bf3489b42a77823872e8669
Igor Patro <tools@skyagency360.pl>
2021-11-07 10:32:05 +0100

    Created working schema

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,41 @@
-const handler = (req, res) => {
-  res.status(200).send("test")
+import { PrismaClient } from "@prisma/client"
+import bcrypt from "bcrypt"
+
+const prisma = new PrismaClient()
+
+const hashPassword = async (password: string): Promise<string> =>
+  await bcrypt.hash(password, 10).then((hash) => hash)
+
+const comparePassword = async (password: string, hash: string) =>
+  await bcrypt.compare(password, hash).then((result) => result)
+
+const handler = async (req, res) => {
+  const users = await prisma.user.findMany({
+    include: {
+      Cart: true,
+    },
+  })
+
+  await prisma.user.create({
+    data: {
+      email: "i.patro@wp.pl",
+      passwordHash: await hashPassword("pass123"),
+      Cart: {
+        create: {
+          products: [
+            {
+              productId: "iddddd",
+              productPrice: 25,
+              productQuantity: 2,
+            },
+          ],
+          sum: 50,
+        },
+      },
+    },
+  })
+
+  res.status(200).json({ users })
 }
 
 export default handler
